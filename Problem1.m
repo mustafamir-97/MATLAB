5cff9f13636d8e126da3cab5614a0e80a6016605
mustafamir-97 <55333913+mustafamir-97@users.noreply.github.com>
2019-09-14 17:47:53 -0500

    Add files via upload

--- Problem1.m
+++ Problem1.m
@@ -1,53 +1,48 @@
 %{
-Mustafa Hafeez Mir
-MAE 2360-003
-Lab 7
-Question 1
+Name: Mustafa Hafeez Mir
+Class: MAE 2360-003
+Assignment: Lab 3 Problem 1
 %}
 
-close all;
-clear all;                  %% Standard Housekeeping
-clc
+function main
+clc                             %% Standard Housekeeping
+clear
+close all
 
-a = 0 : 0.1 : 5;            %% an array declared and initialized that has values of t
-n = length(a);              %% calculates the length of this array and stores in n
+fprintf('*************Problem 1 done using while loop************* \n\n'); %% First Part of problem
 
-for re = 1 : 1 : n
-    fd_x(re) = 0;
-    bd_x(re) = 0;           %% Pre-allocating these arrays that are used later
-    cd_x(re) = 0;           %% The size here is also declared by the for loop conditions
-    analytical(re) = 0;     %% fd is forward difference, bd is backward difference, cd is central difference
-end
+n1 = input('Input the number which you want factorial of: ' );      %% Prompts user to enter number they want increment of
+fprintf('\n');
 
-for p = 1 : 1 : length(a)
-    f_x(p) = (10*cos(2*a(p))*(exp(-0.8*a(p))));     %% the function given values of t and computed here and stored in f_x
-end
+factorial(n1);          %% Calls factorial function with the value of the number that has been initialized by user
 
-h = a(2) - a(1);            %% dt calculated andd stored in h
+fprintf('\n*************Problem 1 done using Recursion************* \n\n'); %% Second Part of Problem
 
-for q = 1 : 1 : n-1
-    fd_x(q) = (f_x(q+1) - f_x(q))/(h);          %% forward difference method, appropriate loop condition apply
-    fd_x(51) = 0;
-end
+n2 = input('Input the number which you want factorial of: ' );          %% Prompts user to enter number they want increment of
 
-for r = 2 : 1 : n
-    bd_x(r) = (f_x(r-1) - f_x(r))/(h);      %% backward difference method, appropriate loop conditions apply
-    bd_x(1) = 0;
-end
+fprintf('\nThe value of %0.0d! is %0.0d. \n', n2, Recursion(n2));           %% The value of the factorial displayed
 
-for s = 2 : 1 : n-1
-    cd_x(s) = (f_x(s+1) - f_x(s-1))/(2*h);
-    cd_x(1) = 0;
-    cd_x(51) = 0;                           %% central difference method, appropriate loop conditions apply
-end
+end             %% End of this function
 
-for u = 1 : 1 : n
-    analytical(u) = -1*(20*sin(2*a(u))+8*cos(2*a(u)))/(exp(0.8*a(u)));      %% analytical, can be done to entire series
-end
+function OUT = factorial(n1)            %% function declared for calculating the factorial using while loop
+temp = n1;          %% value of the number factorial needed of stored in a seperate variable for later display
+product = 1;        %% a local variable product declared and initialized
+    while (n1 > 0)      %% While loop started. Stop condition is if n1 reaches zero
+    product = product * n1;             %% The computation for factorial
+    n1 = n1 - 1;            %% n1 decremented
+    end
 
-fprintf('\tt\t\t\t \tf(t) \t\t Analytical f''(t)\t Forward f''(t)\t\t Backward f''(t)\t\t\tCentral f''(t) \n');     %% Table heading label
+fprintf('The value of %0.0f! is %0.0f \n', temp, product);          %% Output statement displayed
 
-for t = 1 : 1 : n
-fprintf('\t%0.1f\t\t\t %2.6f \t\t %2.6f \t\t\t %2.6f \t\t\t\t %2.6f \t\t\t%2.6f\n', [a(t); f_x(t); analytical(t); fd_x(t); bd_x(t); cd_x(t)]);
-end
-%% Values printed                                 
+end             %% End of function
+
+function OUT = Recursion(n2)            %% function declared for calculating the factorial using recursion
+
+    if n2 == 1              %% If increments reaches 1
+        OUT = 1;            %% The output from the function is 1
+    else
+        OUT = n2 * Recursion(n2 - 1);           %% The output from the function is product of n2 and the function again with -1 the current n2.
+    end
+
+
+end         %% End of function
